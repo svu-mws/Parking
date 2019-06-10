387f9dae0aefe67698da9bd0896935f62f899cce
Zaher Airout <m.zaher.airout@gmail.com>
2019-06-10 08:15:39 +0300

    initial commit

---
+++ ADP Project Final/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model;
+using System.Data.Entity;
+
+namespace ADP_Project_Final
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Database.SetInitializer(new DataBaseInitializer());
+
+            ParkingManagerDatabaseEntities db = new ParkingManagerDatabaseEntities();
+            //db.Customers.Add(new Customer());
+            db.Customers.Add(new Customer() {Name = "Ahmad", ID = 1213213});
+            db.SaveChanges();
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Model/DataBaseInitializer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class DataBaseInitializer : DropCreateDatabaseIfModelChanges<DB_Context>
+    {
+        protected override void Seed(DB_Context context)
+        {
+            context.Customers.Add(new Customer() { Name = "Salem" });
+            context.SaveChanges();
+        }
+    }
+
+}
